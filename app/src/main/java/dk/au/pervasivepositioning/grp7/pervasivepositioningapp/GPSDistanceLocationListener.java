b4842531b9036f908f4699150a86f4b85d3ea4b1
Andreas Malling Østergaard <andreas.malling@gmail.com>
2016-10-03 15:22:49 +0200

    Herp derp

---
+++ app/src/main/java/dk/au/pervasivepositioning/grp7/pervasivepositioningapp/GPSDistanceLocationListener.java
@@ -0,0 +1,61 @@
+package dk.au.pervasivepositioning.grp7.pervasivepositioningapp;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GPSDistanceLocationListener implements LocationListener {
+
+    private Activity activity;
+    private int distance;
+    private Location previousLocation;
+
+    public GPSDistanceLocationListener(Activity activity, int distance) {
+        this.activity = activity;
+        this.distance = distance;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        try {
+            if (previousLocation == null || loc.distanceTo(previousLocation) >= distance) {
+                previousLocation = loc;
+
+                DateFormat format = new SimpleDateFormat("HH:mm:ss");
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File( sdCard.getAbsolutePath(), "PervPos");
+                dir.mkdirs();
+                File file = new File(dir, "gps2-distance.txt");
+                FileWriter writer = null;
+                writer = new FileWriter(file, true);
+                writer.append(format.format(new Date()) + " ");        //hh:mm:ss
+                writer.append(loc.getLatitude() + " ");
+                writer.append(loc.getLongitude() + "\n");
+                writer.close();
+            } else {
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+}
--- app/src/main/java/dk/au/pervasivepositioning/grp7/pervasivepositioningapp/GPSPeriodicLocationListener.java
+++ app/src/main/java/dk/au/pervasivepositioning/grp7/pervasivepositioningapp/GPSPeriodicLocationListener.java
@@ -5,45 +5,45 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static android.content.ContentValues.TAG;
 
-public class GPSLocationListener implements LocationListener {
+public class GPSPeriodicLocationListener implements LocationListener {
 
     private Activity activity;
 
-    public GPSLocationListener(Activity activity) {
+    public GPSPeriodicLocationListener(Activity activity) {
         this.activity = activity;
     }
 
     @Override
     public void onLocationChanged(Location loc) {
-
         try {
-            FileWriter writer = new FileWriter("gps-coordinates.txt");
-            writer.append(System.currentTimeMillis() + " ");
+            DateFormat format = new SimpleDateFormat("HH:mm:ss");
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File( sdCard.getAbsolutePath(), "PervPos");
+            dir.mkdirs();
+            File file = new File(dir, "gps1-periodic.txt");
+            FileWriter writer = null;
+            writer = new FileWriter(file, true);
+            writer.append(format.format(new Date()) + " ");        //hh:mm:ss
             writer.append(loc.getLatitude() + " ");
             writer.append(loc.getLongitude() + "\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Toast.makeText(
-                activity,
-                "Location changed: Lat: " + loc.getLatitude() + " Lng: "
-                        + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-        String longitude = "Longitude: " + loc.getLongitude();
-        Log.v(TAG, longitude);
-        String latitude = "Latitude: " + loc.getLatitude();
-        Log.v(TAG, latitude);
     }
 
     @Override
